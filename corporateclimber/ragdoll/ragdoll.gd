01fde1a2daac6b496076010fd6286c15f20aa9ad
Darren Inouye <dinouye@scu.edu>
2024-08-18 12:27:14 -0700

    Climbing Pegs

--- corporateclimber/ragdoll/ragdoll.gd
+++ corporateclimber/ragdoll/ragdoll.gd
@@ -31,6 +31,7 @@ var right_hand_grab = null
 @onready var left_grab_joint: PinJoint3D = $"Skeleton3D/PhysicalBoneSimulator3D/Physical Bone mixamorig_LeftForeArm/GrabJoint"
 @onready var right_grab_joint: PinJoint3D = $"Skeleton3D/PhysicalBoneSimulator3D/Physical Bone mixamorig_RightForeArm/GrabJoint"
 
+var ragdoll_mode = false
 
 func _ready() -> void:
 	$Skeleton3D/PhysicalBoneSimulator3D.physical_bones_start_simulation()
@@ -43,8 +44,9 @@ func _process(delta: float) -> void:
 	HandleGrab()
 
 func HandleGrab():
-	if Input.is_action_pressed("left_mouse"):
-		left_arm_control.rotation.x = camera_root.rotation.x * 2
+	if Input.is_action_pressed("left_mouse") and not ragdoll_mode:
+		if not Input.is_action_pressed("freeze_left"):
+			left_arm_control.rotation.x = camera_root.rotation.x * 2
 		left_hand_active = true
 		
 		left_arm_control.get_node("Generic6DOFJoint3D").set_flag_x(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, true)
@@ -65,8 +67,9 @@ func HandleGrab():
 		left_grab_joint.set_node_b("")
 		left_hand_grab = null
 	
-	if Input.is_action_pressed("right_mouse"):
-		right_arm_control.rotation.x = camera_root.rotation.x * 2
+	if Input.is_action_pressed("right_mouse") and not ragdoll_mode:
+		if not Input.is_action_pressed("freeze_right"):
+			right_arm_control.rotation.x = camera_root.rotation.x * 2
 		right_hand_active = true
 		
 		right_arm_control.get_node("Generic6DOFJoint3D").set_flag_x(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, true)
@@ -88,42 +91,82 @@ func HandleGrab():
 		right_hand_grab = null
 
 func HandleRotation():
+	if ragdoll_mode:
+		return
 	body_control.rotation.y = camera_root.rotation.y
 	left_leg_control.rotation.y = camera_root.rotation.y
 	right_leg_control.rotation.y = camera_root.rotation.y
-	left_arm_control.rotation.y = camera_root.rotation.y
-	right_arm_control.rotation.y = camera_root.rotation.y
+	if not Input.is_action_pressed("freeze_left"):
+		left_arm_control.rotation.y = camera_root.rotation.y
+	if not Input.is_action_pressed("freeze_right"):
+		right_arm_control.rotation.y = camera_root.rotation.y
 
 func HandleWalk():
 	is_walking = false
-	for bone in body_bones:
-		if Input.is_action_pressed("forward"):
-			bone.apply_central_impulse(-body_control.transform.basis.z * WALK_SPEED / body_bones.size())
-			is_walking = true
-		if Input.is_action_pressed("left"):
-			bone.apply_central_impulse(-body_control.transform.basis.x * WALK_SPEED / body_bones.size())
-			is_walking = true
-		if Input.is_action_pressed("right"):
-			bone.apply_central_impulse(body_control.transform.basis.x * WALK_SPEED / body_bones.size())
-			is_walking = true
-		if Input.is_action_pressed("backward"):
-			bone.apply_central_impulse(body_control.transform.basis.z * WALK_SPEED / body_bones.size())
-			is_walking = true
-		
-		if Input.is_action_just_pressed("jump"):
-			if can_jump:
-				if jump_ray_cast_3d.is_colliding():
-					if jump_ray_cast_3d.get_collision_normal().y > 0.5:
-						can_jump = false
-						bone.apply_central_impulse(body_control.transform.basis.y * JUMP_STRENGTH / body_bones.size())
-						await get_tree().create_timer(0.25).timeout
-						can_jump = true
+	if not ragdoll_mode:
+		for bone in body_bones:
+			if Input.is_action_pressed("forward"):
+				bone.apply_central_impulse(-body_control.transform.basis.z * WALK_SPEED / body_bones.size())
+				is_walking = true
+			if Input.is_action_pressed("left"):
+				bone.apply_central_impulse(-body_control.transform.basis.x * WALK_SPEED / body_bones.size())
+				is_walking = true
+			if Input.is_action_pressed("right"):
+				bone.apply_central_impulse(body_control.transform.basis.x * WALK_SPEED / body_bones.size())
+				is_walking = true
+			if Input.is_action_pressed("backward"):
+				bone.apply_central_impulse(body_control.transform.basis.z * WALK_SPEED / body_bones.size())
+				is_walking = true
+			
+			if Input.is_action_just_pressed("jump"):
+				if can_jump:
+					if jump_ray_cast_3d.is_colliding():
+						if jump_ray_cast_3d.get_collision_normal().y > 0.5:
+							can_jump = false
+							bone.apply_central_impulse(body_control.transform.basis.y * JUMP_STRENGTH / body_bones.size())
+							await get_tree().create_timer(0.25).timeout
+							can_jump = true
 	
 	if is_walking:
 		AnimateWalk()
 	else:
 		left_leg_control.rotation.x = 0
 		right_leg_control.rotation.x = 0
+
+func HandleRagdoll():
+	var value = not ragdoll_mode
+	right_arm_control.get_node("Generic6DOFJoint3D").set_flag_x(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	right_arm_control.get_node("Generic6DOFJoint3D").set_flag_y(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	right_arm_control.get_node("Generic6DOFJoint3D").set_flag_z(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	right_arm_control.get_node("Generic6DOFJoint3D2").set_flag_x(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	right_arm_control.get_node("Generic6DOFJoint3D2").set_flag_y(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	right_arm_control.get_node("Generic6DOFJoint3D2").set_flag_z(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	left_arm_control.get_node("Generic6DOFJoint3D").set_flag_x(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	left_arm_control.get_node("Generic6DOFJoint3D").set_flag_y(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	left_arm_control.get_node("Generic6DOFJoint3D").set_flag_z(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	left_arm_control.get_node("Generic6DOFJoint3D2").set_flag_x(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	left_arm_control.get_node("Generic6DOFJoint3D2").set_flag_y(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	left_arm_control.get_node("Generic6DOFJoint3D2").set_flag_z(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	right_leg_control.get_node("Generic6DOFJoint3D").set_flag_x(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	right_leg_control.get_node("Generic6DOFJoint3D").set_flag_y(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	right_leg_control.get_node("Generic6DOFJoint3D").set_flag_z(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	right_leg_control.get_node("Generic6DOFJoint3D2").set_flag_x(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	right_leg_control.get_node("Generic6DOFJoint3D2").set_flag_y(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	right_leg_control.get_node("Generic6DOFJoint3D2").set_flag_z(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	left_leg_control.get_node("Generic6DOFJoint3D").set_flag_x(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	left_leg_control.get_node("Generic6DOFJoint3D").set_flag_y(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	left_leg_control.get_node("Generic6DOFJoint3D").set_flag_z(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	left_leg_control.get_node("Generic6DOFJoint3D2").set_flag_x(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	left_leg_control.get_node("Generic6DOFJoint3D2").set_flag_y(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	left_leg_control.get_node("Generic6DOFJoint3D2").set_flag_z(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	body_control.get_node("Generic6DOFJoint3D").set_flag_x(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	body_control.get_node("Generic6DOFJoint3D").set_flag_z(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	body_control.get_node("Generic6DOFJoint3D2").set_flag_x(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	body_control.get_node("Generic6DOFJoint3D2").set_flag_y(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	body_control.get_node("Generic6DOFJoint3D2").set_flag_z(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	body_control.get_node("Generic6DOFJoint3D3").set_flag_x(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	body_control.get_node("Generic6DOFJoint3D3").set_flag_y(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
+	body_control.get_node("Generic6DOFJoint3D3").set_flag_z(Generic6DOFJoint3D.FLAG_ENABLE_ANGULAR_SPRING, value)
 
 func AnimateWalk():
 	walk_animation_timer += 0.1
@@ -135,6 +178,10 @@ func _input(event: InputEvent) -> void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	if Input.is_action_just_pressed("ragdoll"):
+		ragdoll_mode = not ragdoll_mode
+		HandleRagdoll()
 	
 	if event is InputEventMouseMotion:
 		camera_root.rotation_degrees.y -= event.relative.x * MOUSE_SENSITIVITY
@@ -149,6 +196,7 @@ func _on_RightHand_body_entered(body: Node3D) -> void:
 				right_grab_joint.set_node_a(right_hand.get_path())
 				right_grab_joint.set_node_b(body.get_path())
 				right_hand_grab = body
+				print("Right hand grabbing " + body.name)
 
 
 func _on_LeftHand_body_entered(body: Node3D) -> void:
@@ -158,3 +206,4 @@ func _on_LeftHand_body_entered(body: Node3D) -> void:
 				left_grab_joint.set_node_a(left_hand.get_path())
 				left_grab_joint.set_node_b(body.get_path())
 				left_hand_grab = body
+				print("Left hand grabbing " + body.name)
